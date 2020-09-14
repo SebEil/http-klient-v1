0d392c4e5d9773bc88f414ffe5036c9af9d1a282
SebEil <56046884+SebEil@users.noreply.github.com>
2020-09-14 16:56:42 +0200

    Dev-2020-09 Complete parsing of QueryString (#1)
    * New failing test
    
    * New failing test "shouldRetrieveParameterByName"
    
    * New failing test "shouldHandleMultipleParameters"
    
    * All tests are green

--- src/test/java/QueryStringTest.java
+++ src/test/java/QueryStringTest.java
@@ -9,4 +9,29 @@ public class QueryStringTest {
         QueryString queryString = new QueryString("Status=200");
         assertEquals("200", queryString.getParameter("Status"));
     }
+
+    @Test
+    void shouldRetrieveOtherQueryParameters(){
+        QueryString queryString = new QueryString("Status=404");
+        assertEquals("404", queryString.getParameter("Status"));
+    }
+
+    @Test
+    void shouldRetrieveParameterByName(){
+        QueryString queryString = new QueryString("text=hello");
+        assertEquals(null, queryString.getParameter("Status"));
+        assertEquals("hello", queryString.getParameter("text"));
+    }
+
+    @Test
+    void shouldHandleMultipleParameters(){
+        QueryString queryString = new QueryString("text=hello&status=200");
+        assertEquals("200", queryString.getParameter("status"));
+        assertEquals("hello", queryString.getParameter("text"));
+    }
+
+
+
+
+
 }
